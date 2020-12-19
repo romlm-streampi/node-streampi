d793b1308ab9e77b8aa95879382187c45bb13538
romlm <romain103paris@gmail.com>
2020-12-19 23:48:18 +0100

    implement script configuration

--- model/script-info.ts
+++ model/script-info.ts
@@ -1,7 +1,14 @@
 
+export interface ScriptParameter {
+	name: string,
+	type: "string" | "provided" | "number",
+	description: string | undefined,
+	provider: string | undefined,
+	value: any | undefined
+}
 
 export default interface ScriptInfo {
 	name: string,
 	category: string,
-	parameters?: {}
+	parameters?: ScriptParameter[]
 }
