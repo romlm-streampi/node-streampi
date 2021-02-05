e4f9b935977e5c8fe6fc6146c4af4c820d507207
romlm <romain103paris@gmail.com>
2021-02-05 19:22:30 +0100

    add  createLayout function in layout.ts

--- src/model/layout.ts
+++ src/model/layout.ts
@@ -1,3 +1,4 @@
+import { nanoid } from "nanoid";
 import { IScriptInstance } from "./script";
 
 
@@ -12,10 +13,20 @@ export interface ILayoutSize {
 	rowNumber: number;
 }
 
+export const LAYOUT_SIZES = {
+	MINI: { colNumber: 3, rowNumber: 2 },
+	DEFAULT: { colNumber: 5, rowNumber: 3 },
+	LARGE: { colNumber: 8, rowNumber: 5 }
+}
+
 export default class Layout {
 	constructor(
 		public readonly id: string,
 		public size: ILayoutSize,
 		public positioners: IPositioner[]
 	) { }
+}
+
+export function createLayout(size = LAYOUT_SIZES.DEFAULT, positioners = []) {
+	return new Layout(nanoid(), size, positioners);
 }
