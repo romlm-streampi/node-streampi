88d29af167af941167d923828b1adc135da87681
romlm <romain103paris@gmail.com>
2021-02-14 12:09:33 +0100

    add barebones of script picker

--- src/components/script-picker/index.tsx
+++ src/components/script-picker/index.tsx
@@ -1,86 +1,82 @@
 import { PluginComponent } from "@model/plugin-export";
+import { isEqual } from "lodash";
 import { nanoid } from "nanoid";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./script-picker.module.scss";
-
-const Pane = ({ category, expanded = false, children, onClick }: { category: string, expanded?: boolean, onClick?: any, children?: JSX.Element[] | JSX.Element }) => {
-
-	const ref = useRef<HTMLDivElement>();
-
-	if (expanded) {
-		const timer = setInterval(() => {
-			if (ref) {
-				ref.current.classList.toggle(styles.expanded, true);
-				clearInterval(timer);
-			}
-		}, 10);
-	}
-
-	return (<div className={styles.pane}>
-		<span className={styles.title} onClick={onClick}>{category}</span>
-		<div className={styles.content} ref={ref}>
-			{children}
-		</div>
-	</div>)
-
-}
-
 
 interface IProps {
 	plugins: PluginComponent[];
-	onPluginPicked: (plugin: PluginComponent) => void;
+	onPluginPicked: (plg: PluginComponent) => void;
 }
 
-export default function ScriptPicker({ plugins, onPluginPicked }: IProps): JSX.Element {
-
-	const [expandedPane, setExpandedPane] = useState<string | undefined>();
-
-	const onChangeExpanded = (category: string) => {
-		if (category !== expandedPane)
-			setExpandedPane(category)
-		else
-			setExpandedPane(undefined);
+const sortCategories = (a: string, b: string): number => {
+	if (a === "others") {
+		return 10;
+	} else if (b === "others") {
+		return -10;
+	} else if (a < b) {
+		return -1;
+	} else if (a > b) {
+		return 1;
 	}
+	return 0;
+}
 
-	const orderedPlugins = {};
-	for (let plg of plugins) {
-		const category = plg.descriptor.info.category || "others";
-		if (Object.values(orderedPlugins).includes(category)) {
-			orderedPlugins[category].push(plg);
-		} else {
-			orderedPlugins[category] = [plg];
-		}
-	}
 
-	const sortCategories = (a: string, b: string): number => {
-		if (a === "others") {
-			return 10;
-		} else if (b === "others") {
-			return -10;
-		} else if (a < b) {
-			return -1;
-		} else if (a > b) {
-			return 1;
-		}
-		return 0;
-	}
+export default function ScriptPicker({ plugins, onPluginPicked }: IProps) {
 
-	return <div className={styles.container}>
+	const [pickedPlugin, setPickedPlugin] = useState<PluginComponent | undefined>(undefined);
+	const [orderedPlugins, setOrderedPlugins] = useState<any>({});
+
+	useEffect(() => {
+		const categories = {};
+		plugins.forEach((plg) => {
+			const category = plg.descriptor.info.category || "others";
+			if (Object.keys(categories).includes(category)) {
+				categories[category].push(plg)
+			} else {
+				categories[category] = [plg];
+			}
+		});
+
+		setOrderedPlugins(categories);
+	}, [plugins])
+
+
+	return (<div className={styles.container}>
 		{
+
+
 			Object.keys(orderedPlugins).sort(sortCategories).map((category) => {
-				return (
-					<Pane key={nanoid()} category={category} expanded={category === expandedPane} onClick={() => onChangeExpanded(category)}>
-						{
-							orderedPlugins[category].map((plg: PluginComponent) => {
-								return (<div key={nanoid()} className={styles.script} onClick={() => onPluginPicked(plg)}>
-									{plg.descriptor.info.displayName}
-								</div>)
-							})
-						}
-					</Pane>
-				)
+
+				const plgList = orderedPlugins[category];
+
+
+				return (<div key={nanoid()}>
+					<span>{category}</span>
+					{plgList.map((plg) => {
+						const id = nanoid();
+						const { descriptor: { info: { displayName }, id: scriptId } } = plg;
+						return (
+							<span key={id}>
+								<label htmlFor={`script-radio-${id}`}>{displayName}</label>
+								<input
+									type="radio"
+									id={`script-radio-${id}`}
+									value={[scriptId.moduleName, scriptId.script]}
+									onChange={(ev) => ev.target.checked && setPickedPlugin(plg)}
+									name="script"
+									checked={isEqual(plg.descriptor.id, pickedPlugin?.descriptor.id)}
+								/>
+							</span>
+						)
+					})}
+				</div>)
 			})
 		}
-	</div>
 
+		<div>
+			picked plugin: {pickedPlugin?.descriptor.info.displayName}
+		</div>
+	</div>);
 }
